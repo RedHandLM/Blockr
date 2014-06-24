f8e232e9e3d95ad992c292498d53ce62efba86af
Pierre Pierre <pierre.depaz@gmail.com>
2014-06-24 16:19:17 -0400

    Shield roughly working
    yay

--- Blockr/src/blockr/Blockr.java
+++ Blockr/src/blockr/Blockr.java
@@ -35,11 +35,26 @@ public class Blockr extends PApplet {
 	// ***Shield Setup
 	//----------------------------------
 	Shield sh = new Shield();
-	float sRed;
-	float sGreen;
-	float sBlue;
-	float sWeight;
-	float sAlpha;
+	float aRed1 = 255;
+	float aGreen1 = 255;
+	float aBlue1 = 255;
+	float aWeight1 = 2;
+	float aAlpha1 = 100;
+	float aDist1 = 0;
+	
+	float aRed2 = 255;
+	float aGreen2 = 255;
+	float aBlue2 = 255;
+	float aWeight2 = 2;
+	float aAlpha2 = 100;
+	float aDist2 = 0;
+	
+	float aRed3 = 255;
+	float aGreen3 = 255;
+	float aBlue3 = 255;
+	float aWeight3 = 2;
+	float aAlpha3 = 100;
+	float aDist3 = 0;
 	
 	// ***Me Setup
 	//----------------------------------
@@ -92,7 +107,7 @@ public class Blockr extends PApplet {
 		// ***BG
 		//----------------------------------
 		background(0);
-		image (bg, centerX + pmouseX/-1, centerY + pmouseY/-1);
+		image (bg, centerX + menowX/-1, centerY + menowY/-1);
 		  
 		// ***Shield
 		//----------------------------------
@@ -145,9 +160,17 @@ public class Blockr extends PApplet {
 		noStroke();
 		ellipse(mePos.x, mePos.y, 50, 50);
 		noFill();
-		strokeWeight(sWeight);
-		stroke(sRed, sGreen, sBlue, sAlpha);
-		ellipse(mePos.x, mePos.y, 200, 200);
+		strokeWeight(21-((aDist1/PI)*20));
+		stroke(aRed1, aGreen1, aBlue1, aAlpha1);
+		arc(mePos.x, mePos.y, 200.0f, 200.0f, PI-aDist1, PI+aDist1);
+		/*
+		strokeWeight(aWeight2);
+		stroke(aRed2, aGreen2, aBlue2, aAlpha2);
+		arc(mePos.x, mePos.y, 200.0f-aDist2, 200.0f-aDist2, (2*PI)/3, (4*PI)/3);
+		strokeWeight(aWeight3);
+		stroke(aRed3, aGreen3, aBlue3, aAlpha3);
+		arc(mePos.x, mePos.y, 200.0f-aDist3, 200.0f-aDist3, (4*PI)/3, 2*PI);
+		*/
 	}
 	
 	//------------------THIS IS THE FUNCTION GETTING CHANGES IN VALUES FROM THE MIDI CONTROLLER
@@ -157,16 +180,49 @@ public class Blockr extends PApplet {
 		println("Number: "+number);
 		println("Value: "+value);
 		
+		//-------------------FIRST ARC
 		if(number==33){//first fader
-			sRed = map(value, 0, 127, 0, 255);
+			aRed1 = map(value, 0, 127, 0, 255);
 		}else if(number == 34){//second fader
-			sGreen = map(value, 0, 127, 0, 255);
+			aGreen1 = map(value, 0, 127, 0, 255);
 		}else if(number == 35){//third fader
-			sBlue = map(value, 0, 127, 0, 255);
+			aBlue1 = map(value, 0, 127, 0, 255);
 		}else if(number==17){//first knob
-			sWeight = map(value, 127, 0, 1, 50); 
+			aWeight1 = map(value, 127, 0, 1, 30); 
 		}else if(number==18){//second knob
-			sAlpha = map(value, 127, 0, 0, 255);
+			aAlpha1 = map(value, 127, 0, 100, 255);
+		}else if(number == 19){
+			aDist1 = map(value, 127f, 0f, PI, 0f);
+		}
+		
+		//-------------------SECOND ARC
+		if(number==36){//first fader
+			aRed2 = map(value, 0, 127, 0, 255);
+		}else if(number == 37){//second fader
+			aGreen2 = map(value, 0, 127, 0, 255);
+		}else if(number == 38){//third fader
+			aBlue2 = map(value, 0, 127, 0, 255);
+		}else if(number==20){//first knob
+			aWeight2 = map(value, 127, 0, 1, 30); 
+		}else if(number==21){//second knob
+			aAlpha2 = map(value, 127, 0, 100, 255);
+		}else if(number == 22){
+			aDist2 = map(value, 127, 0, 0, 50);
+		}
+		
+		//-------------------THIRD ARC
+		if(number==39){//first fader
+			aRed3 = map(value, 0, 127, 0, 255);
+		}else if(number == 40){//second fader
+			aGreen3 = map(value, 0, 127, 0, 255);
+		}else if(number == 41){//third fader
+			aBlue3 = map(value, 0, 127, 0, 255);
+		}else if(number==23){//first knob
+			aWeight3 = map(value, 127, 0, 1, 30); 
+		}else if(number==24){//second knob
+			aAlpha3 = map(value, 127, 0, 100, 255);
+		}else if(number == 25){
+			aDist3 = map(value, 127, 0, 0, 50);
 		}
 		
 	}
